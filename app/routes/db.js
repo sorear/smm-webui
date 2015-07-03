39be8f1e44635a08e0739ca53379f7ef5df7bf64
Stefan O'Rear <stefanor@cox.net>
2015-07-02 22:56:07 -0700

    add caching for quick navigation between databases, affix_url feature

--- app/routes/db.js
+++ app/routes/db.js
@@ -12,7 +12,14 @@ export default Ember.Route.extend({
     let dir = this.modelFor('application');
     let db = dir.known_databases.filter(db => db.name === params.db)[0];
     if (db) {
-      return parseAsync(db.database_url, url => Ember.RSVP.resolve(Ember.$.get(url))).then(mmom => ({ name: params.db, database: mmom, title: db.title }));
+      if (!db.promise) {
+        let affixp = Ember.RSVP.resolve(db.affix_url ? Ember.$.get(db.affix_url) : '');
+        db.promise = affixp.then(atext =>
+          parseAsync(db.database_url,
+            url => Ember.RSVP.resolve(Ember.$.get(url)).then(text => text + atext)))
+          .then(mmom => ({ name: params.db, database: mmom, title: db.title }));
+      }
+      return db.promise;
     }
     else {
       return Ember.RSVP.reject('No such database'); // TODO how do we error?
